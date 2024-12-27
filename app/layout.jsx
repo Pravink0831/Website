8ee1cf97ea12741c7ec5ad5b44e26d2a05a9312d
Prabhakar Konduskar <pravin.konduskar@accenture.com>
2024-12-28 00:06:22 +0530

    Initial commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -33,6 +33,7 @@ export default function RootLayout({ children }) {
           href="https://fonts.gstatic.com"
           crossOrigin="true"
         />
+        <link href="https://fonts.googleapis.com/css2?family=Dan&display=swap" rel="stylesheet"/>
         <link
           href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500;600&display=swap"
           rel="stylesheet"
