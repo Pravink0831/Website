9d65489dbdd80ee4a2625f054d76212b6024c24f
Pravink0831 <ash112679@gmail.com>
2025-01-15 12:25:35 +0530

    commit

--- netlify/functions/server.jsx
+++ netlify/functions/server.jsx
@@ -6,7 +6,7 @@ const app = express();
 const router = express.Router();
 
 router.use('/uploads', express.static(path.join(__dirname, '../../public/uploads'))); // Adjust the path if necessary
-router.use('/api/upload', require('./upload')); // Adjust the path if necessary
+router.use('/api/upload', require('./api/upload')); // Adjust the path if necessary
 
 app.use('/.netlify/functions/upload', router);
 
--- pages/server.jsx
+++ pages/server.jsx
@@ -1,12 +1,18 @@
+require('dotenv').config();
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
+if (process.env.NODE_ENV !== 'production') {
+  // Local development
+  app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
+  app.use('/api/upload', require('./api/upload'));
 
-app.use('/api/upload', require('./api/upload'));
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+} else {
+  // In production, server logic is handled by Netlify Functions
+  module.exports = app;
+}
