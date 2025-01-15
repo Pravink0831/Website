c369793225b03b6a0aaa70dfd969515fe08ac71e
Pravink0831 <ash112679@gmail.com>
2025-01-15 12:38:46 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, { isServer }) => { if (!isServer) { config.resolve.fallback = { fs: false, net: false }; } return config; },
+    webpack: (config, { isServer }) => { if (!isServer) { config.resolve.fallback = { fs: false, net: false,async_hooks: false }; } return config; },
 }
 
 module.exports = nextConfig
