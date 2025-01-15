5c06b9f9ec4106dea4c551c9240650f1e7922e2b
Pravink0831 <ash112679@gmail.com>
2025-01-15 12:00:23 +0530

    commit

---
+++ netlify/functions/upload.jsx
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const serverless = require("serverless-http");
+
+const app = express();
+const router = express.Router();
+
+router.use('/uploads', express.static(path.join(__dirname, '../../public/uploads'))); // Adjust the path if necessary
+router.use('/api/upload', require('./upload')); // Adjust the path if necessary
+
+app.use('/.netlify/functions/upload', router);
+
+module.exports.handler = serverless(app);
