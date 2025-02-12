b3e00c7f2fdf10cfecd34a91c4ef64956a86153d
Pravink0831 <ash112679@gmail.com>
2025-02-12 12:39:48 +0530

    db commit

--- components/dashboard/vendor-dashboard/add-hotel/components/content/BannerUploader.jsx
+++ components/dashboard/vendor-dashboard/add-hotel/components/content/BannerUploader.jsx
@@ -23,7 +23,7 @@ const BannerUploader = ({ images, setImages }) => {
             const formData = new FormData();
             formData.append("img", file);
 
-            const response = await axios.post("http://localhost:3000/api/upload", formData, {
+            const response = await axios.post("/api/upload", formData, { // Use relative URL
               headers: {
                 "Content-Type": "multipart/form-data",
               },
--- netlify/functions/server.jsx
+++ netlify/functions/server.jsx
@@ -8,6 +8,6 @@ const router = express.Router();
 router.use('/uploads', express.static(path.join(__dirname, '../../public/uploads'))); // Adjust the path if necessary
 router.use('/api/upload', require('./api/upload')); // Adjust the path if necessary
 
-app.use('/.netlify/functions/upload', router);
+app.use('/.netlify/functions/server', router);
 
 module.exports.handler = serverless(app);
